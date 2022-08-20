56723be7c1eb8871b89bde533b3db20049d7cdcd
npsaunders <neilpsaunders@gmail.com>
2022-08-20 12:22:45 -0400

    connect to api and comment out return data

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//declare variables
+//connect to api using ajax
+//create functions to populate the table
+//Clear out the input field
+// //
+// console.log('https://api.maas2.apollorion.com/');
+// ----  Return JSON data
+// {
+//   "status": 200,
+//   "id": 3251,
+//   "terrestrial_date": "2022-03-24T00:00:00.000Z",
+//   "ls": 195,
+//   "season": "Month 7",
+//   "min_temp": -68,
+//   "max_temp": -5,
+//   "pressure": 760,
+//   "pressure_string": "Higher",
+//   "abs_humidity": null,
+//   "wind_speed": null,
+//   "atmo_opacity": "Sunny",
+//   "sunrise": "05:18",
+//   "sunset": "17:22",
+//   "local_uv_irradiance_index": "Moderate",
+//   "min_gts_temp": -66,
+//   "max_gts_temp": 5,
+//   "wind_direction": null,
+//   "sol": 3423,
+//   "unitOfMeasure": "Celsius",
+//   "TZ_Data": "America/Port_of_Spain"}
+//
+//
