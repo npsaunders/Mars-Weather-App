d3eca718c3f8f985a1ced24381283a00283c0c74
npsaunders <neilpsaunders@gmail.com>
2022-08-20 14:30:16 -0400

    commit code before moving all items to new directory

--- js/app.js
+++ js/app.js
@@ -1,31 +1,55 @@
+//console.log JSON return data and comment out. Use to get pieces of data
 //declare variables
 //connect to api using ajax
+//listen for submit button to be clicked
 //create functions to populate the table
+//    do error checking/try-catch-finally-throw
 //Clear out the input field
-// //
+// -- Example --
 // console.log('https://api.maas2.apollorion.com/');
-// ----  Return JSON data
+// ----  Return JSON data   
+//     ** denotes data needed
+//
 // {
 //   "status": 200,
 //   "id": 3251,
 //   "terrestrial_date": "2022-03-24T00:00:00.000Z",
 //   "ls": 195,
-//   "season": "Month 7",
-//   "min_temp": -68,
-//   "max_temp": -5,
+// **  "season": "Month 7",
+// **  "min_temp": -68,
+// **  "max_temp": -5,
 //   "pressure": 760,
 //   "pressure_string": "Higher",
 //   "abs_humidity": null,
 //   "wind_speed": null,
-//   "atmo_opacity": "Sunny",
-//   "sunrise": "05:18",
-//   "sunset": "17:22",
+// **  "atmo_opacity": "Sunny",
+// **  "sunrise": "05:18",
+// **  "sunset": "17:22",
 //   "local_uv_irradiance_index": "Moderate",
 //   "min_gts_temp": -66,
 //   "max_gts_temp": 5,
 //   "wind_direction": null,
-//   "sol": 3423,
+// **  "sol": 3423,
 //   "unitOfMeasure": "Celsius",
 //   "TZ_Data": "America/Port_of_Spain"}
 //
 //
+//  ---- Ensure jQuery loaded first
+$(document).ready(function () {
+
+
+
+  // ----- Variables
+
+
+  // ----- FUNCTIONS
+  //
+
+  // ----- Click Submit Button
+  //Check to see if a value has been entered. If not, do nothing
+
+
+
+
+  //close jQuery loaded function
+});
