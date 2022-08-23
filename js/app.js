2ba6ce58e177e4e1029aee7774f2a59e9833cc98
npsaunders <neilpsaunders@gmail.com>
2022-08-23 09:19:21 -0400

    code cleanup

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
-//console.log JSON return data and comment out. Use to get pieces of data
-//declare variables
-//connect to api using ajax
-//listen for submit button to be clicked
-//create functions to populate the table
-//    do error checking/try-catch-finally-throw
-//Clear out the input field
+//This is the JSON return data from the API.  Use to get pieces of data
 // -- Example --
 // console.log('https://api.maas2.apollorion.com/');
 // ----  Return JSON data   
@@ -36,17 +30,25 @@
 //
 
 // Show the main content if JavaScript is enabled.
-// If JavaScript can't be loaded, only show the error image
+// If JavaScript can't be loaded, only show the <noscript> error image
 $('#main').css('display', 'flex');
 
-//get today's date in the form mm/dd/yyyy
-const maxDate = new Date().toISOString().slice(0, 10);
+//get today's date in the form mm/dd/yyyy. This will be used to set the calendar date picker's last valid date that can be selected (no future dates).
+const caldrMaxDate = new Date().toISOString().slice(0, 10);
 
-//now set the max date attribute in the date picker
-$('#start').attr("max", maxDate);
+//now set the max date attribute in the date picker.
+$('#start').attr("max", caldrMaxDate);
+
+
+// ---------  VARIABLES  ---------
+
+//set maxDate to today's date. 
+const maxDate = new Date();
+
+//Curiosity start date = '2012-08-05' = 0 sols
+const missionStartDate = new Date('2012-08-05');
 
 let marsWeatherData, userInput;
-
 const $earthDt = $('#earthDt');
 const $min_temp = $('#minTemp');
 const $max_temp = $('#maxTemp');
@@ -59,12 +61,14 @@ const $solNum = $('#solNum');
 
 $('form').on('submit', returnDataAndDisplay);
 
+
 function returnDataAndDisplay(event) {
   //prevents page refresh after clicking the submit button
   event.preventDefault();
-  userInput = $inputSol.val();
-  console.log('datae entere', userInput, typeof (userInput))
 
+  //convert user input of yyyy-dd-mm to Martian sols.
+  // console.log('$inputSol', new Date($inputSol.val()).getTime());
+  userInput = terrestrialToSols($inputSol.val());
 
   // AJAX call to get data
   $.ajax({
@@ -83,12 +87,24 @@ function returnDataAndDisplay(event) {
 }
 
 function updateDom() {
+  //only get the date in yyyy/mm/dd format
   $earthDt.text(marsWeatherData.terrestrial_date.slice(0, 10));
-  $min_temp.text(marsWeatherData.min_temp);
-  $max_temp.text(marsWeatherData.max_temp);
+  //Temps are in Deg C.  use the \xBOC for degree symbol
+  $min_temp.text(marsWeatherData.min_temp + '\xB0C');
+  $max_temp.text(marsWeatherData.max_temp + '\xB0C');
   $sunrise.text(marsWeatherData.sunrise);
   $sunset.text(marsWeatherData.sunset);
   $atmCond.text(marsWeatherData.atmo_opacity);
   $season.text(marsWeatherData.season);
   $solNum.text(marsWeatherData.sol);
+}
+
+// Purpose: Take the user entered Earth date yyyy-mm-dd format and convert it to sols
+// Param string of 'yyyy-mm-dd'
+// Returns a string in Martian sols
+function terrestrialToSols(enteredDate) {
+  //first convert the date entered into milliseconds and subtract the 2 times to get elapsed time in milliseconds
+  const elapsedTime = new Date(enteredDate).getTime() - missionStartDate.getTime();
+  //convert milliseconds to Martian sols. Key difference is 1 sol is 24.65972 hours
+  return Math.floor((elapsedTime / (1000 * 3600 * 24.65972222)));
 }
