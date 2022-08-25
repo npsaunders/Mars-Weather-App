f1c86e5c869e76b720d28e99015e885dc445c40c
npsaunders <neilpsaunders@gmail.com>
2022-08-25 13:05:01 -0400

    make sure jQuery is loaded

--- js/app.js
+++ js/app.js
@@ -59,89 +59,92 @@ const $season = $('#season');
 const $inputSol = $('input[type="date"]');
 const $solNum = $('#solNum');
 
+//Make sure jQuery is loaded
+$(document).ready(function () {
 
-$('form').on('submit', returnDataAndDisplay);
+  $('form').on('submit', returnDataAndDisplay);
 
-//  ---------  FUNCTIONS  ---------
+  //  ---------  FUNCTIONS  ---------
 
-function returnDataAndDisplay(event) {
-  //prevents page refresh after clicking the submit button
-  event.preventDefault();
+  function returnDataAndDisplay(event) {
+    //prevents page refresh after clicking the submit button
+    event.preventDefault();
 
-  //convert user input of yyyy-dd-mm to Martian sols.\
-  userInput = terrestrialToSols($inputSol.val());
+    //convert user input of yyyy-dd-mm to Martian sols.\
+    userInput = terrestrialToSols($inputSol.val());
 
-  // AJAX call to get data
-  $.ajax({
-    url: 'https://api.maas2.apollorion.com/' + userInput
-  }).then(
-    // if ok then continue to update the DOM
-    (data) => {
-      marsWeatherData = data;
-      updateDom();
-    },
-    // If there's no data or an issue, just put in N/A for the values
-    (error) => {
-      $earthDt.text('Error');
-      $min_temp.text('No');
-      $max_temp.text('Data');
-      $sunrise.text('Available');
-      $sunset.text('For');
-      $atmCond.text('That');
-      $season.text('Date');
-      $solNum.text('  ');
-      // alert("There was an issue with data retrieval.")
-    }
-  );
-}
-
-function updateDom() {
-  //only get the date in yyyy/mm/dd format
-  $earthDt.text(marsWeatherData.terrestrial_date.slice(0, 10));
-  //Temps are in Deg C.  use the \xBOC for degree symbol
-  $min_temp.text(marsWeatherData.min_temp + '\xB0C');
-  $max_temp.text(marsWeatherData.max_temp + '\xB0C');
-  $sunrise.text(marsWeatherData.sunrise);
-  $sunset.text(marsWeatherData.sunset);
-  $atmCond.text(marsWeatherData.atmo_opacity);
-  $season.text(marsWeatherData.season);
-  $solNum.text(marsWeatherData.sol);
-}
-
-// Purpose: Take the user entered Earth date yyyy-mm-dd format and convert it to sols
-// Param string of 'yyyy-mm-dd'
-// Returns a string in Martian sols
-function terrestrialToSols(enteredDate) {
-  //first convert the date entered into milliseconds and subtract the 2 times to get elapsed time in milliseconds
-  const elapsedTime = new Date(enteredDate).getTime() - missionStartDate.getTime();
-  //convert milliseconds to Martian sols. Key difference is 1 sol is 24.65972 hours
-  return Math.floor((elapsedTime / (1000 * 3600 * 24.65972222)));
-}
-
-//Change the h3 text when carousel button clicked
-//To do this, need to listen for a carousel button to be clicked.
-//Since there is a 0.6 second transition, need to set a timer
-//to assure the new slide is visible before putting the text
-
-//Listen for carousel button click. 
-//If clicked, delay 0.7secs before calling the function to change the h3 text.
-$('#carouselBut').click(function () {
-  setTimeout(clickCarBut, 700);
-})
-
-//Set the h3 text based on which slide is visible
-function clickCarBut() {
-  if ($('#tableForm').is(":visible")) {
-    $('#line1').text("Curiosity Rover Historical Weather Readings");
-    $('#line2').text("Utilizing the MAAS2 REST API");
-  } else if ($('#carRover').is(":visible")) {
-    $('#line1').text(" Rover Environmental Monitoring Station (REMS)");
-    $('#line2').text("Courtesy NASA / JPL - Caltech");
-  } else if ($('#weatherChart').is(":visible")) {
-    $('#line1').text("Weather Chart");
-    $('#line2').text("Courtesy NASA / JPL - Caltech");
-  } else {
-    $('#line1').text("Season Chart");
-    $('#line2').text("Courtesy NASA / JPL - Caltech");
+    // AJAX call to get data
+    $.ajax({
+      url: 'https://api.maas2.apollorion.com/' + userInput
+    }).then(
+      // if ok then continue to update the DOM
+      (data) => {
+        marsWeatherData = data;
+        updateDom();
+      },
+      // If there's no data or an issue, just put in N/A for the values
+      (error) => {
+        $earthDt.text('Error');
+        $min_temp.text('No');
+        $max_temp.text('Data');
+        $sunrise.text('Available');
+        $sunset.text('For');
+        $atmCond.text('That');
+        $season.text('Date');
+        $solNum.text('  ');
+        // alert("There was an issue with data retrieval.")
+      }
+    );
   }
-}
+
+  function updateDom() {
+    //only get the date in yyyy/mm/dd format
+    $earthDt.text(marsWeatherData.terrestrial_date.slice(0, 10));
+    //Temps are in Deg C.  use the \xBOC for degree symbol
+    $min_temp.text(marsWeatherData.min_temp + '\xB0C');
+    $max_temp.text(marsWeatherData.max_temp + '\xB0C');
+    $sunrise.text(marsWeatherData.sunrise);
+    $sunset.text(marsWeatherData.sunset);
+    $atmCond.text(marsWeatherData.atmo_opacity);
+    $season.text(marsWeatherData.season);
+    $solNum.text(marsWeatherData.sol);
+  }
+
+  // Purpose: Take the user entered Earth date yyyy-mm-dd format and convert it to sols
+  // Param string of 'yyyy-mm-dd'
+  // Returns a string in Martian sols
+  function terrestrialToSols(enteredDate) {
+    //first convert the date entered into milliseconds and subtract the 2 times to get elapsed time in milliseconds
+    const elapsedTime = new Date(enteredDate).getTime() - missionStartDate.getTime();
+    //convert milliseconds to Martian sols. Key difference is 1 sol is 24.65972 hours
+    return Math.floor((elapsedTime / (1000 * 3600 * 24.65972222)));
+  }
+
+  //Change the h3 text when carousel button clicked
+  //To do this, need to listen for a carousel button to be clicked.
+  //Since there is a 0.6 second transition, need to set a timer
+  //to assure the new slide is visible before putting the text
+
+  //Listen for carousel button click. 
+  //If clicked, delay 0.7secs before calling the function to change the h3 text.
+  $('#carouselBut').click(function () {
+    setTimeout(clickCarBut, 700);
+  })
+
+  //Set the h3 text based on which slide is visible
+  function clickCarBut() {
+    if ($('#tableForm').is(":visible")) {
+      $('#line1').text("Curiosity Rover Historical Weather Readings");
+      $('#line2').text("Utilizing the MAAS2 REST API");
+    } else if ($('#carRover').is(":visible")) {
+      $('#line1').text(" Rover Environmental Monitoring Station (REMS)");
+      $('#line2').text("Courtesy NASA / JPL - Caltech");
+    } else if ($('#weatherChart').is(":visible")) {
+      $('#line1').text("Weather Chart");
+      $('#line2').text("Courtesy NASA / JPL - Caltech");
+    } else {
+      $('#line1').text("Season Chart");
+      $('#line2').text("Courtesy NASA / JPL - Caltech");
+    }
+  }
+})
